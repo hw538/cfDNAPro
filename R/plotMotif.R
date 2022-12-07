bc8dfebd385e1b761263ef55ae5323fcc3c61e4d
haichao <wanghaichao2014@gmail.com>
2022-12-07 19:16:47 +0000

    ggplot complicance in a package

--- R/plotMotif.R
+++ R/plotMotif.R
@@ -3,7 +3,7 @@
 plotMotif <- function(x){
   
   
-  p <- ggplot(x, aes(motif, fraction)) +
+  p <- ggplot(x, aes(.data$motif, .data$fraction)) +
     geom_col() +
     theme_classic() +
     theme(axis.text.x = element_text(angle = 50, hjust = 0.5, vjust = 0.5))
