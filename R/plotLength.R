2661d516db98a9b390c6f8c3d2e82643649c8ec4
haichao <wanghaichao2014@gmail.com>
2022-12-07 23:05:07 +0000

    dynamic x axis breaking and labelling

--- R/plotLength.R
+++ R/plotLength.R
@@ -2,10 +2,11 @@
 plotLength <- function(x,
                        plot_type = c("fraction"),
                        xlim = c(30, 500),
-                       vline = c(167),
-                       ylim = NA_real_ ,
-                       x_breaks = c(50, 100, 167, 200, 300, 400 , 500),
-                       x_labels = c("50", "100", "167", "200", "300", "400", "500"),
+                       ylim  ,
+                       x_breaks,
+                       x_labels,
+                       vline,
+                       add_vline = TRUE,
                        line_color = "grey1",
                        line_size = 0.8,
                        line_alpha = 0.8,
@@ -22,38 +23,88 @@ plotLength <- function(x,
   plot_type <-  stringr::str_to_lower(plot_type)
   max_count <- max(dplyr::pull(x, All_Reads.fr_count))
   max_fraction <- max(dplyr::pull(x, prop ))
-  
 
   
   if(plot_type == c("fraction")) {
-    ylim <- c(0, max_fraction + 0.0015)
+    
     y_use <- "prop"
+    
+    if(missing(ylim)) {
+    ylim <- c(0, max_fraction * 1.1)
+    message("ylim", " was set as: ", ylim[1]  ," - ", ylim[2] )
+    }
+    
+    
   } else if (plot_type == c("count")) {
-    ylim <- c(0, max_count + 1000)
     y_use <- "All_Reads.fr_count"
+    
+    if(missing(ylim)) {
+    ylim <- c(0, max_count * 1.2)
+    
+    message("ylim", " was set as: ", ylim[1]  ," - ", ylim[2] )
+    
+    }
+    
   }
     
   x$insert_size <- as.numeric(x$insert_size)
   plot_type <- stringr::str_to_title(plot_type)
   
-      
-
-      p <-  ggplot(x) +
-        geom_line(aes(.data$insert_size, .data[[y_use]]), 
-                  color = line_color, 
-                  size = line_size, 
-                  alpha = line_alpha) +
-        geom_vline(xintercept = vline, linetype = vline_type, color = vline_color, size = vline_size) + 
-        coord_cartesian(xlim = xlim) +
-        labs(x = "cfDNA Fragment Length (bp)", y = plot_type) +
-        scale_x_continuous(limits = xlim, 
-                           breaks = x_breaks, 
-                           labels = x_labels) + 
-        scale_y_continuous(expand = c(0, 0)) +
-        theme_length_plot()
+  # find the modal length 
+    peak_isize_breaks <- dplyr::filter(x, !!rlang::sym(y_use) == max(x[[y_use]])) %>%
+      dplyr::pull(.data$insert_size)
+    
+  # define the vline as modal length
+  if(missing(vline)){
+    vline <- as.vector(peak_isize_breaks)
+  }
+  
+  # set the x breaks 
+  if(missing(x_breaks) & missing(x_labels)) {
+    ref_breaks <- seq(100, 1000, by = 100)
+    
+    i <- which(ref_breaks > xlim[1] & ref_breaks < xlim[2])
+    middle_isize_breaks <- ref_breaks[i]
+    
+    x_breaks <- c(xlim[1], middle_isize_breaks, peak_isize_breaks, xlim[2]) %>% 
+      sort()
+    x_labels <- as.character(x_breaks)
+  }
+  
+  if(!missing(x_breaks) & missing(x_labels)) {
+    x_labels <- as.character(x_breaks)
+  }
+  
+  if(missing(x_breaks) & !missing(x_labels)) {
+    x_breaks <- as.numeric(x_labels)
+  }
+  
+  # plot
+  p <-  ggplot(x) +
+    geom_line(aes(.data$insert_size, .data[[y_use]]), 
+              color = line_color, 
+              size = line_size, 
+              alpha = line_alpha)
+  
+  if(add_vline) {
+    p <- p +
+    geom_vline(xintercept = vline, linetype = vline_type, color = vline_color, size = vline_size)
+  }
+  
+  p <- p + 
+    coord_cartesian(xlim = xlim) +
+    labs(x = "cfDNA Fragment Length (bp)", y = plot_type) +
+    scale_x_continuous(limits = xlim, 
+                       breaks = x_breaks, 
+                       labels = x_labels) + 
+    scale_y_continuous(limits = ylim) +
+    theme_length_plot()
+  
 
       
       
       return(p)
   
 }
+
+
