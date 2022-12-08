74aacfdc933c2f30d923d8b46c652398e9e1de83
haichao <wanghaichao2014@gmail.com>
2022-12-08 23:32:00 +0000

    much better plotMotif and themes

--- R/plotMotif.R
+++ R/plotMotif.R
@@ -1,16 +1,66 @@
 
 
-plotMotif <- function(x){
+plotMotif <- function(x,
+                      ylim,
+                      x_title = "Motif",
+                      plot_type = c("Fraction"),
+                      bar_color = c("A" = "cornflowerblue",
+                                     "C" = "darksalmon",
+                                     "G" = "palevioletred2",
+                                     "T" = "forestgreen"),
+                      motif_levels = c("C", "G", "A", "T"),
+                      ...){
+  
+  y_use <- NULL
+  x <- tibble::as_tibble(x)
+  plot_type <-  stringr::str_to_lower(plot_type)
+  max_count <- max(dplyr::pull(x, .data$n))
+  max_fraction <- max(dplyr::pull(x, .data$fraction ))
   
   
+  if(plot_type == c("fraction")) {
+    
+    y_use <- "fraction"
+    
+    if(missing(ylim)) {
+      ylim <- c(0, max_fraction * 1.1)
+      message("ylim", " was set as: ", ylim[1]  ," - ", ylim[2] )
+    }
+    
+    
+  } else if (plot_type == c("n")) {
+    y_use <- "n"
+    
+    if(missing(ylim)) {
+      ylim <- c(0, max_count * 1.2)
+      
+      message("ylim", " was set as: ", ylim[1]  ," - ", ylim[2] )
+      
+    }
+    
+  }
+  
+  x <- x %>%
+    dplyr::mutate(group  = stringr::str_extract(.data$motif, pattern = "^[ATCG]")) %>%
+    dplyr::filter(.data$group %in% motif_levels)
+  
+  x$group <- factor(x$group, levels = motif_levels)
+  
+  x <- x %>%
+    dplyr::arrange(.data$group)
+  
+  x$motif <-  factor(x$motif, levels = x$motif)
   
   
-  
-  p <- ggplot(x, aes(.data$motif, .data$fraction)) +
+  p <- ggplot(data = x, 
+              mapping = aes(x = .data$motif, 
+                            y =  .data[[y_use]], 
+                            fill = .data$group)) +
     geom_col() +
-    theme_classic() +
-    theme(axis.text.x = element_text(angle = 50, hjust = 0.5, vjust = 0.5))
-  
+    labs(x = x_title, y = stringr::str_to_title(plot_type)) +
+    scale_fill_manual(values = bar_color) +
+    scale_y_continuous(limits = ylim) +
+    theme_motif_plot()
   return(p)
   
 }
--- R/themes.R
+++ R/themes.R
@@ -4,7 +4,18 @@ theme_length_plot <- function(){
   theme_classic() %+replace%
     theme(
       axis.text = element_text(size = 10),
-      axis.text.x = element_text(angle = 45, hjust = 0.5, vjust = 0.5),
+      axis.text.x = element_text(angle = 45, hjust = 0.5, vjust = 0.5, size = 8),
+      axis.title = element_text(size = 14, face = "bold"),
+      legend.position = "none"
+    )
+}
+
+theme_motif_plot <- function(){
+
+  theme_classic() %+replace%
+    theme(
+      axis.text = element_text(size = 10),
+      axis.text.x = element_text(angle = 45, hjust = 0.5, vjust = 0.5, size = 8),
       axis.title = element_text(size = 14, face = "bold"),
       legend.position = "none"
     )
@@ -15,5 +26,6 @@ theme_cnv_plot <- function(){
   theme_classic() %+replace%
     theme(axis.ticks.x = element_blank(),
         axis.line.x = element_blank(),
-        axis.title = element_text(face = "bold"))
+      axis.text.x = element_text(size = 8),
+        axis.title = element_text(size = 14, face = "bold"))
 }
