babc7493120592ccdf4003a1b0755edd150c0522
haichao <wanghaichao2014@gmail.com>
2022-12-08 20:03:18 +0000

    added call segment step

--- R/callCNV.R
+++ R/callCNV.R
@@ -1,5 +1,4 @@
 
-bam <- "/Users/wang04/Documents/phd_projects/cfdnapro_debug/SLX-11379_D703_D504.mrkdup.BL.filtered.bam.markduplicates.bam.downsamp_0.1x.mrkdup.bam"
 
 callCNV <- function(bamfile,
                     bin_size = 1000,
@@ -17,7 +16,8 @@ callCNV <- function(bamfile,
     QDNAseq::normalizeBins() %>%
     QDNAseq::smoothOutlierBins() %>%
     QDNAseq::segmentBins(transformFun="sqrt") %>%
-    QDNAseq::normalizeSegmentedBins()
+    QDNAseq::normalizeSegmentedBins() %>%
+    QDNAseq::callBins()
   
   return(seg)
 }
