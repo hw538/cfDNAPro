d7b7a57157723f343f18023248054e3fd01d7d13
haichao <wanghaichao2014@gmail.com>
2023-01-06 16:38:21 +0000

    added customize count

--- R/summarizeBam.R
+++ R/summarizeBam.R
@@ -1,16 +1,30 @@
 
-
-###############################################################################
-# summariseBam function
-###############################################################################
-
 #' Summarise descriptive Bam stats
 #'
-#' @param bamfile 
-#' @param total_count 
-#' @param total_mapped_count 
+#' @param bamfile A Bam file
+#' @param total_count Boolean. default = TRUE, which means calculating the total 
+#' number of reads. 
+#' @param total_mapped_count Boolean. Default = TRUE, which mean calculating the
+#' number of mapped reads.
 #' @param chrM_count 
 #' @param duplicate_count 
+#' @param coverage_by_mapped 
+#' @param genome_length_bp 
+#' @param customized_count 
+#' @param customized_count_mapqFilter 
+#' @param customized_count_isPaired 
+#' @param customized_count_isProperPair 
+#' @param customized_count_isUnmappedQuery 
+#' @param customized_count_hasUnmappedMate 
+#' @param customized_count_isMinusStrand 
+#' @param customized_count_isMateMinusStrand 
+#' @param customized_count_isFirstMateRead 
+#' @param customized_count_isSecondMateRead 
+#' @param customized_count_isNotPrimaryRead 
+#' @param customized_count_isSecondaryAlignment 
+#' @param customized_count_isNotPassingQualityControls 
+#' @param customized_count_isDuplicate 
+#' @param customized_count_isSupplementaryAlignment 
 #' @param ... 
 #'
 #' @return tibble object (i.e. a dataframe)
@@ -31,6 +45,20 @@ summarizeBam <- function(bamfile = NULL,
                          duplicate_count = TRUE,
                          coverage_by_mapped = TRUE,
                          genome_length_bp = 3200000000,
+                         customized_count = FALSE,
+                         customized_count_mapqFilter=NA_integer_,
+                         customized_count_isPaired = NA, 
+                         customized_count_isProperPair = NA, 
+                         customized_count_isUnmappedQuery = NA, 
+                         customized_count_hasUnmappedMate = NA, 
+                         customized_count_isMinusStrand = NA, 
+                         customized_count_isMateMinusStrand = NA,
+                         customized_count_isFirstMateRead = NA, 
+                         customized_count_isSecondMateRead = NA, 
+                         customized_count_isSecondaryAlignment = NA, 
+                         customized_count_isNotPassingQualityControls = NA,
+                         customized_count_isDuplicate = NA, 
+                         customized_count_isSupplementaryAlignment = NA,
                          ...) {
   
   
@@ -106,6 +134,37 @@ summarizeBam <- function(bamfile = NULL,
     message("Calculate coverage...")
     summary_metrics <- dplyr::mutate(summary_metrics, 
                                      coverage_by_mapped_reads = n_nucleotide_mapped / !!genome_length_bp )
+  }
+  
+  
+  if(customized_count) {
+    
+    message("Count reads with customized filtering criteria...")
+    
+    flag_customized <- scanBamFlag(isPaired = customized_count_isPaired, 
+                        isProperPair = customized_count_isProperPair, 
+                        isUnmappedQuery = customized_count_isUnmappedQuery, 
+                        hasUnmappedMate = customized_count_hasUnmappedMate, 
+                        isMinusStrand = customized_count_isMinusStrand, 
+                        isMateMinusStrand = customized_count_isMateMinusStrand,
+                        isFirstMateRead = customized_count_isFirstMateRead, 
+                        isSecondMateRead = customized_count_isSecondMateRead, 
+                        isSecondaryAlignment = customized_count_isSecondaryAlignment, 
+                        isNotPassingQualityControls = customized_count_isNotPassingQualityControls,
+                        isDuplicate = customized_count_isDuplicate, 
+                        isSupplementaryAlignment = customized_count_isSupplementaryAlignment)
+    
+    mapqFilter_customized <- customized_count_mapqFilter
+    
+    param <- Rsamtools::ScanBamParam(flag = flag_customized,
+                                     mapqFilter = mapqFilter_customized)
+    
+    customized_count <- bam_count(bamfile = bamfile, param = param) %>%
+      dplyr::rename(n_read_customized = n_read, 
+                    n_nucleotide_customized = n_nucleotide)
+    
+    summary_metrics <- dplyr::full_join(summary_metrics, customized_count, 
+                                        by = "file")
   }
   
   message("Done!")
